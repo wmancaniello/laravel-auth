70d9a88398df7a048c76fe6570370d3c5fa78d88
wmancaniello <wmancaniello@gmail.com>
2024-06-20 18:30:01 +0200

    fix table

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -3,14 +3,18 @@
 @section('content')
     <div class="container mt-4 gap-5">
         <h1>Lista Progetti:</h1>
-        <a class="btn btn-success my-2" href="{{ route('admin.projects.create') }}">Nuovo progetto</a>
-        <table>
+        <a class="btn btn-success my-2" href="{{ route('admin.projects.create') }}">
+            <i class="fa-solid fa-plus"></i> Nuovo progetto
+        </a>
+
+        <table class="table">
             <thead>
                 <tr>
                     <th scope="col">ID</th>
                     <th scope="col">Titolo</th>
                     <th scope="col">Descrizione</th>
                     <th scope="col">Slug</th>
+                    <th scope="col">Azione</th>
                 </tr>
             </thead>
             <tbody>
